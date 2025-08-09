a921bf98f4cdb5c4345a78b1bba007302b3099a2
Suneeh <suneeh1338@gmail.com>
2025-08-09 23:22:22 +0200

    layout done-ish

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [MatCardModule, MatIconModule, MatDividerModule],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
