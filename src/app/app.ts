a8b7fc61d8cea8fa2513c7ca2e27f5b0277d74cf
Suneeh <suneeh1338@gmail.com>
2025-08-10 18:52:03 +0200

    add projects

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,4 +11,43 @@ import { MatDividerModule } from '@angular/material/divider';
 })
 export class App {
   protected readonly title = signal('portfolio');
+  blogEntries: BlogListEntry[] = [
+    {
+      icon: 'book',
+      title: 'Database Optimization',
+      subtitle: 'Write Optimization vs. Read Optimization',
+      href: 'ööhm foo bar',
+    },
+    {
+      icon: 'book',
+      title: 'Atmoic Habits',
+      subtitle: 'My summary of James Clears bestseller',
+      href: 'ööhm foo bar',
+    },
+    {
+      icon: 'book',
+      title: 'Domain Driven Design',
+      subtitle: "My summary of Amichai Mantinband's course",
+      href: 'ööhm foo bar',
+    },
+    {
+      icon: 'book',
+      title: 'Rate Limiting',
+      subtitle: 'How to protect your APIs',
+      href: 'ööhm foo bar',
+    },
+    {
+      icon: 'book',
+      title: 'Tree Structured Data',
+      subtitle: 'How to setup your Database correctly',
+      href: 'ööhm foo bar',
+    },
+  ];
+}
+
+interface BlogListEntry {
+  icon: string;
+  title: string;
+  subtitle: string;
+  href: string;
 }
